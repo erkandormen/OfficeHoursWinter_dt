d61c794bf9321cb0c8c133ae5b9f3115a743a6a4
erkandormen <ekstremyazilim@gmail.com>
2021-03-18 23:34:46 +0300

    Output Formatting change

--- src/day06/Office_1_OutputFormatting.java
+++ src/day06/Office_1_OutputFormatting.java
@@ -40,15 +40,26 @@ public class Office_1_OutputFormatting {
 		each integer that was less than 3 digits now has leading zeroes.
 		 */
 		
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("================================");
-		for(int i=0; i<3;i++) {
-			String s = scan.next();
-			int x = scan.nextInt();
-			System.out.printf("%-15s%03d",s,x);
- 		}
-		
-	}
+            Scanner sc=new Scanner(System.in);
+            String [] s = new String[3];
+            Integer [] n = new Integer[3];
+            for(int i=0;i<3;i++)
+            {
+                s[i]=sc.next();
+                n[i]=sc.nextInt();
+            }
+            
+            System.out.println("================================");
+            
+            for(int i=0; i<3;i++){
+                System.out.printf("%-15s%03d\n", s[i],n[i]);
+            }
+            
+            System.out.println("================================");
+            
+            sc.close();
+
+    }
 
 }
